b868ca94e79c6af0239a17376bfd8e4dbb7d8217
sdn145 <mandasukaaku@gmail.com>
2026-09-10 07:40:29 +0000

    Clean repo: keep README.md only

--- js/audio.js
+++
@@ -1,4 +0,0 @@
-Zuxcyn.audioTracks=[];
-function importAudio(file){const url=URL.createObjectURL(file);Zuxcyn.audioTracks.push({name:file.name,type:file.type,size:file.size,url});toast("Audio ditambahkan")}
-function createAudioInput(){const input=document.createElement("input");input.type="file";input.accept="audio/*";input.onchange=()=>input.files[0]&&importAudio(input.files[0]);input.click()}
-window.importAudio=importAudio;window.createAudioInput=createAudioInput;
